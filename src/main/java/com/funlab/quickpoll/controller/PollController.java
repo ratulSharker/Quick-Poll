fe085f486f3e87a398737edabc3bc197efb2fdac
Ratul sharker <sharker.ratul.08@gmail.com>
2020-10-20 18:41:47 +0600

    Get all polls rest api prepared.

--- src/main/java/com/funlab/quickpoll/controller/PollController.java
+++ src/main/java/com/funlab/quickpoll/controller/PollController.java
@@ -1,8 +1,13 @@
 package com.funlab.quickpoll.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.funlab.quickpoll.domain.Poll;
 import com.funlab.quickpoll.repositoy.PollRepository;
 
 @RestController
@@ -11,7 +16,11 @@ public class PollController {
 	@Autowired
 	private PollRepository pollRepository;
 
+	@RequestMapping(value = "/polls", method = RequestMethod.GET)
+	public ResponseEntity<Iterable<Poll>> getAllPolls() {
+		return new ResponseEntity<Iterable<Poll>>(this.pollRepository.findAll(), HttpStatus.OK);
+	}
 	
 	
-	
+
 }
