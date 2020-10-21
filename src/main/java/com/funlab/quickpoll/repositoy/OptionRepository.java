df273adef2e9591ac9eceeaae566780e4022f6b7
Ratul Sharker <Sharker.ratul.08@gmail.com>
2020-10-22 01:19:28 +0600

    Repository annotation attached.

--- src/main/java/com/funlab/quickpoll/repositoy/OptionRepository.java
+++ src/main/java/com/funlab/quickpoll/repositoy/OptionRepository.java
@@ -1,9 +1,12 @@
 package com.funlab.quickpoll.repositoy;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.funlab.quickpoll.domain.Option;
 
+
+@Repository
 public interface OptionRepository extends CrudRepository<Option, Long> {
 
 }
