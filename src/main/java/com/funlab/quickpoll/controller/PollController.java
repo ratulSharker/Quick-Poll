6f9f70fcf325fd390d5e1cf6f9830eb975c9aaeb
Ratul sharker <sharker.ratul.08@gmail.com>
2020-10-21 11:43:07 +0600

    Following things are done 1. Get particular poll endpoint prepared. 2. Update particular poll endpoint prepared. 3. Delete particular poll endpoint prepared.

--- src/main/java/com/funlab/quickpoll/controller/PollController.java
+++ src/main/java/com/funlab/quickpoll/controller/PollController.java
@@ -1,11 +1,14 @@
 package com.funlab.quickpoll.controller;
 
 import java.net.URI;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,16 +32,37 @@ public class PollController {
 	@RequestMapping(value = "/polls", method = RequestMethod.POST)
 	public ResponseEntity<Poll> createPoll(@RequestBody Poll poll) {
 		poll = this.pollRepository.save(poll);
-		
+
 		HttpHeaders responseHeaders = new HttpHeaders();
-		URI uri = ServletUriComponentsBuilder
-					.fromCurrentRequest()
-					.path("/{id}")
-					.buildAndExpand(poll.getId())
-					.toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(poll.getId()).toUri();
 		responseHeaders.setLocation(uri);
-		
+
 		return new ResponseEntity<Poll>(poll, responseHeaders, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/polls/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Poll> getPoll(@PathVariable("id") Long pollId) {
+
+		try {
+			Optional<Poll> nullablePoll = this.pollRepository.findById(pollId);
+			return new ResponseEntity<Poll>(nullablePoll.get(), HttpStatus.OK);
+		} catch (NoSuchElementException ex) {
+			return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	
+	@RequestMapping(value = "/polls/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Poll> updatePoll(@PathVariable("id") Long pollId, @RequestBody Poll poll) {
+		poll.setId(pollId);
+		poll = this.pollRepository.save(poll);
+		return new ResponseEntity<Poll>(poll, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/polls/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deletePoll(@PathVariable("id") Long pollId) {
+		this.pollRepository.deleteById(pollId);
+		return new ResponseEntity(HttpStatus.OK);
 	}
 
 }
