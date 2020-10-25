6c06e35d967756f7a2c9a9deb30612bd7431f017
Ratul Sharker <Sharker.ratul.08@gmail.com>
2020-10-25 13:19:23 +0600

    Default page size provided via web configuration.

---
+++ src/main/java/com/funlab/quickpoll/configuration/WebMVCConfiguration.java
@@ -0,0 +1,27 @@
+package com.funlab.quickpoll.configuration;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMVCConfiguration implements WebMvcConfigurer {
+
+	
+	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+
+		PageableHandlerMethodArgumentResolver phmar = new PageableHandlerMethodArgumentResolver();
+		phmar.setFallbackPageable(PageRequest.of(0, 5));		
+		resolvers.add(phmar);
+		
+		WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+	}
+	
+}
