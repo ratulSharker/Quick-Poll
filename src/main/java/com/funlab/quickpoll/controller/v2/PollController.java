f2cd49684a8e511e5733e16a561fce36507bca55
Ratul sharker <sharker.ratul.08@gmail.com>
2020-10-27 12:07:08 +0600

    Following things are done
    1. Delete poll endpoint is only authorized for admin users.

--- src/main/java/com/funlab/quickpoll/controller/v2/PollController.java
+++ src/main/java/com/funlab/quickpoll/controller/v2/PollController.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -125,6 +126,7 @@ public class PollController {
 
 	@ApiOperation(value = "Delete a particular poll")
 	@RequestMapping(value = "/polls/{id}", method = RequestMethod.DELETE)
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<Void> deletePoll(@PathVariable("id") Long pollId) {
 		verifyAndGetPoll(pollId);
 		this.pollRepository.deleteById(pollId);
