0cef39d45b55dfaa4b0e9824cbd731bcb63eeece
Ratul sharker <sharker.ratul.08@gmail.com>
2020-10-27 11:49:57 +0600

    Following things are done
    1. V2 is now secured over basic stateless authentication.

--- src/main/java/com/funlab/quickpoll/configuration/SecurityConfiguration.java
+++ src/main/java/com/funlab/quickpoll/configuration/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package com.funlab.quickpoll.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -18,4 +20,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+		.authorizeRequests()
+			.antMatchers("/v1/**", "/swagger-ui/**", "/api-docs/**").permitAll()
+			.antMatchers("/v2/**").authenticated()
+			.and()
+		.httpBasic()
+			.realmName("Quick Poll")
+			.and()
+		.csrf()
+			.disable();		
+	}
+
+	
 }
